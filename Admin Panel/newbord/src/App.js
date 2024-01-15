79e4b2592c85091eca8018916ccb106f38ddc6af
sachin31344 <105095798+sachin31344@users.noreply.github.com>
2024-01-15 10:41:05 +0530

    Admin Panel
    Push Admin Panel

---
+++ Admin Panel/newbord/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+         
+          href="http://127.0.0.1:5000"
+      
+          
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
